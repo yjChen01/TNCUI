2b0043e9c5af695aa0a37a69f79040033950c0c0
yjChen01 <u12321u12321@gmail.com>
2022-11-29 16:25:58 +0800

    儲位顯示

--- src/app/storage-status/storage-status.component.ts
+++ src/app/storage-status/storage-status.component.ts
@@ -16,10 +16,8 @@ export class StorageStatusComponent {
   layer_list_ddl: any[] = [];
 
   //shuttle同層樓僅有一台，arr長度=row數
-  shuttle_row: number;
   shuttle_col: number[] = new Array(this.row_count);
-  //TODO:把它變成[]
-  shuttle_col_state: number = 0;
+  shuttle_col_state: number[] = new Array(this.row_count);
 
   current_layer: number = 1;
 
@@ -75,36 +73,58 @@ export class StorageStatusComponent {
   checkLayerHaveShuttle(EQ: EQStatus) {
     let shuttle: Shuttle[] = [];
     let hasshuttle = 0;
+    //取得所有shuttle陣列
     for (const [key, value] of Object.entries(EQ.shuttles)) {
       shuttle.push(value);
     }
     // console.log(shuttle);
-    let layer_shuttle_array:number[][]=[[]];
+    let layer_shuttle_array: number[][] = [[]];
+    let temp_shuttle_state_arr: number[] = [0, 0, 0, 0, 0];
+    let temp_column_arr: number[] = [-99, -99, -99, -99, -99];
+
+    //檢查是否有shuttle在目標樓層，若有則將其row,col,state加入layer_shuttle_array[]中
     for (let i = 0; i < shuttle.length; i++) {
       if (shuttle[i].layer == this.current_layer) {
-        console.log('shuttle:'+i+';col:'+shuttle[i].column);
+        console.log('shuttle:' + i + ';col:' + shuttle[i].column);
 
-        layer_shuttle_array.push([shuttle[i].row,shuttle[i].column]);
+        layer_shuttle_array.push([
+          shuttle[i].row,
+          shuttle[i].column,
+          shuttle[i].is_error,
+        ]);
         console.log(layer_shuttle_array);
 
-        this.shuttle_row = shuttle[i].row;
-        this.shuttle_col_state = shuttle[i].is_error;
+        // this.shuttle_row = shuttle[i].row;
+        // this.shuttle_col_state = shuttle[i].is_error;
 
-        let shuttle_col:number=shuttle[i].column;
-        for (let j = 0; j < this.shuttle_col.length; j++) {
-          if (j === this.shuttle_row) {
-            this.shuttle_col[j] = shuttle_col;
-          } else {
-            this.shuttle_col[j] = -99;
-          }
-        }
-        console.log(this.shuttle_col);
+        // let shuttle_col:number=shuttle[i].column;
+        // for (let j = 0; j < this.shuttle_col.length; j++) {
+        //   if (j === this.shuttle_row) {
+        //     this.shuttle_col[j] = shuttle_col;
+        //   } else {
+        //     this.shuttle_col[j] = -99;
+        //   }
+        // }
+        // console.log(this.shuttle_col);
+
+        //並計算本樓共有幾台shuttle
         hasshuttle++;
       }
-      // else{
-      //   console.log('不相等:'+shuttle[i].layer+'??'+this.current_layer);
-      // }
     }
+    //若該樓層存在梭車，則對取出的layer_shuttle_arr處理，將值指入顯示用的arr中(layer_shuttle_array[0]是空陣列，故從1開始)
+    if (layer_shuttle_array.length > 1) {
+      for (let j = 1; j < layer_shuttle_array.length; j++) {
+        //將col指入對應row
+        temp_column_arr[layer_shuttle_array[j][0]] = layer_shuttle_array[j][1];
+        //將state指入對應row
+        temp_shuttle_state_arr[layer_shuttle_array[j][0]] = layer_shuttle_array[j][2];
+      }
+    }
+    //最後將值放入實際使用的顯示陣列中
+    this.shuttle_col=temp_column_arr;
+    this.shuttle_col_state=temp_shuttle_state_arr;
+    console.log('col:'+this.shuttle_col);
+    console.log('state:'+this.shuttle_col_state);
     return hasshuttle;
   }
 
@@ -137,17 +157,7 @@ export class StorageStatusComponent {
     http
       .post<EQStatus>('http://192.168.214.87:9080/get_status', '')
       .subscribe((data_result) => {
-        // console.log(this.shuttle_col);
-        let haveshuttle: number = 0;
-        haveshuttle = this.checkLayerHaveShuttle(data_result);
-        // console.log(
-        //   'layer' + this.current_layer + 'have shuttle:' + haveshuttle
-        // );
-        if (haveshuttle === 0) {
-          for (let i = 0; i < this.shuttle_col.length; i++) {
-            this.shuttle_col[i] = -99;
-          }
-        }
+        this.checkLayerHaveShuttle(data_result);
       });
   }
 }
