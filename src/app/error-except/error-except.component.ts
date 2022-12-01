e621b27f1e3def226ffa370b8a0ec2cc44a0ef7c
yjChen01 <u12321u12321@gmail.com>
2022-12-01 16:34:38 +0800

    分頁搜尋完成

--- src/app/error-except/error-except.component.ts
+++ src/app/error-except/error-except.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Bill, JobBill } from '../job-bill';
 
 @Component({
   selector: 'app-error-except',
@@ -7,12 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./error-except.component.css']
 })
 export class ErrorExceptComponent  {
-  data: any[]=[];
+  data: Bill[]=[];
   page_no=1;
   page_size=10;
   isAll=false;
   search_job_id:string='';
-  total_line_count:number=100;
+  total_line_count:number;
+
 
   constructor(private http:HttpClient) {
     this.GetErrorBill(http,'');
@@ -32,14 +34,24 @@ export class ErrorExceptComponent  {
       "isAllData":this.isAll,
       "job_id":v_job_id
     };
-    http.post<any[]>('https://localhost:7285/GetAllBill', body, options).subscribe(data_result=>{
-      this.data=data_result;
+    http.post<JobBill>('https://localhost:7285/GetAllBill', body, options).subscribe(data_result=>{
+      this.data=data_result.bills;
+      this.total_line_count=data_result.total_page_count;
     })
   }
 
   SearchByJobid(v_job_id:string){
     // console.log(v_job_id);
+    this.search_job_id=v_job_id;
+    this.page_no=1;
     this.GetErrorBill(this.http,v_job_id);
   }
+
+  SearchByPageNo(v_page_no:number){
+    this.page_no=v_page_no;
+    this.GetErrorBill(this.http,this.search_job_id);
+    console.log(v_page_no);
+  }
+
 
 }
