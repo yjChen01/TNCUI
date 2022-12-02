cc3273c33d9cc680e244ee5f91138f79b1218e46
yjChen01 <u12321u12321@gmail.com>
2022-12-02 20:39:53 +0800

    api 串接ˋˇˊ

---
+++ src/app/state-modal/state-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-state-modal',
+  templateUrl: './state-modal.component.html',
+  styleUrls: ['./state-modal.component.css']
+})
+export class StateModalComponent  {
+
+  constructor() { }
+
+
+
+}
