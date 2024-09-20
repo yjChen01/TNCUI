a02d3c27878d75b29737ef331ce5be8c8421b2ba
yjChen01 <u12321u12321@gmail.com>
2024-09-20 10:31:46 +0800

    config change

---
+++ src/environments/environment.ia.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  api: 'http://192.168.1.218:5000',
+  geneapi:'http://192.168.100.33:9080',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://192.168.100.33:5000',
+  api: 'http://192.168.1.218:5000',
   geneapi:'http://192.168.100.33:9080',
 };
 
